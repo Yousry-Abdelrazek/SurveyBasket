9839429848dae1de686ffb1cff36afcb851bc58d
Yousry-Abdelrazek <yousryabdelrazek726@gmail.com>
2026-02-04 18:34:04 +0200

    Custom Validation Using Fluent Validation

--- SurveyBasket/Contracts/Validations/CreatePollRequestValidator.cs
+++ SurveyBasket/Contracts/Validations/CreatePollRequestValidator.cs
@@ -6,6 +6,15 @@ public class CreatePollRequestValidator : AbstractValidator<CreatePollRequest>
     public CreatePollRequestValidator()
     {
         RuleFor(x => x.Title)
-            .NotEmpty().WithMessage("Title is required.");
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .Length(3, 100);
+
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("{PropertyName} is required.")
+            .Length(3, 1000);
     }
+
+    // PlaceHolders for future validation rules
+    // ex => {PropertyName} , {PropertyValue}, {MinLength}, {MaxLength}
 }
--- SurveyBasket/Models/Student.cs
+++ SurveyBasket/Models/Student.cs
@@ -6,6 +6,6 @@ public class Student
     public string FirstName { get; set; } = string.Empty;
     public string MiddleName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    [MinAge(18 )]
+    //[MinAge(18 )]
     public DateTime? DateOfBirth { get; set; }
 }
